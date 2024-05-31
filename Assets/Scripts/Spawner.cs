fd9cad44278c33c5176e13df82e214e4447d7949
AboveAverageGames <tjpayne97@gmail.com>
2024-05-31 01:08:00 +0100

    Froze Position on Y Axis for Objects to Avoid bug

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,26 +5,65 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
 
-    public float spawnTimer = 5f;
+    public float spawnTimer;
+    private float spawnTimerStored;
+
+
+    private float randomItemToBeSpawned;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Stores spawn timer
+        spawnTimerStored = spawnTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("The spawn timer is " + spawnTimer);
-        spawnTimer = spawnTimer - Time.deltaTime;
-
-        if (spawnTimer < 0) 
+        //Starts Spawn timer
+        SpawnTimer();
+        if (spawnTimer < 0)
         {
-            spawnLog();
-            spawnTimer = 5;
+            
+            //spawnLog();
+
+            RandomSpawner();
+
+            //Resets Spawn Timer
+            spawnTimer = spawnTimerStored;
         }
     }
+
+
+
+
+    //This chooses a random number between 1 and 3, and then Each value decides what item is spawned
+    // 1: Log, 2:Food 3:Pollution
+    void RandomSpawner()
+    {
+        randomItemToBeSpawned = Random.Range(1, 4);
+
+        if (randomItemToBeSpawned == 1)
+        {
+            spawnLog();
+        }
+        else if (randomItemToBeSpawned == 2)
+        {
+            spawnFood();
+        }
+        else if (randomItemToBeSpawned == 3)
+        {
+            spawnPollution();
+        }
+    }
+
+    //Spawn Timer
+    void SpawnTimer()
+    {
+        spawnTimer = spawnTimer - Time.deltaTime;
+    }
+
 
     void spawnLog()
     {
@@ -34,6 +73,29 @@ public class Spawner : MonoBehaviour
         {
           log.transform.position = transform.position;
             log.SetActive(true);
+        }
+    }
+
+    void spawnFood()
+    {
+        GameObject food = ObjectPool.instance.GetPooledFood();
+
+        if (food != null)
+        {
+            food.transform.position = transform.position;
+            food.SetActive(true);
+        }
+    }
+
+
+    void spawnPollution()
+    {
+        GameObject pollution = ObjectPool.instance.GetPooledPollution();
+
+        if (pollution != null)
+        {
+            pollution.transform.position = transform.position;
+            pollution.SetActive(true);
         }
     }
 }
